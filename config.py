7e9c4c2379a9ceb226571675c8ea6d2e13ffa47c
Jnbots <158847629+Jnbots@users.noreply.github.com>
2024-02-05 06:09:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,84 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6907718633:AAFV34Susg09AnYI6sknozRAEWnHvlkSM6s")
+
+# Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "15191874"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "3037d39233c6fad9b80d83bb8a339a07")
+
+# Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002118263220"))
+
+# OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "5597521952"))
+
+# Port
+PORT = os.environ.get("PORT", "8080")
+
+# Database
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Rename_1by1_robot:THPvSGPzbDdxQ4DU@cluster0.r7y7j.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Rename_1by1_robot")
+
+# force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002029287584"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+#shortner
+SHORTLINK_DOMAIN = os.environ.get('SHORTLINK_DOMAIN', "MoneyKamalo.com")
+SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
+
+# start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+try:
+    ADMINS = []
+    for x in (os.environ.get("ADMINS", "5597521952").split()):
+        ADMINS.append(int(x))
+except ValueError:
+    raise Exception("Your Admins list does not contain valid integers.")
+
+# Force sub message
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+
+# set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+# set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+# Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+# Image URL for lazy loading
+LAZY_PIC = "https://graph.org/file/0fff9567e54efc6cc87a4.jpg"
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(1250450587)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
